80564c93e977ff57f50407bda1c0384010a4f25f
Piotr Krzaczkowski <slimhannya@gmail.com>
2023-05-27 16:17:17 +0200

    feat(backend): add local api, prettier, angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { MatButtonModule } from '@angular/material/button';
+
+const AppImports: Array<any> = [CommonModule, RouterOutlet, MatButtonModule];
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: AppImports,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'medium';
-}
+export class AppComponent {}
